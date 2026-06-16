5d36b570e27c3a340f7a5d7533a559eec4ef2b23
Constantin Saguin <wolfthemes14@gmail.com>
2026-06-16 18:50:36 +0200

    style: marquee style

--- patterns/marquee.php
+++ patterns/marquee.php
@@ -8,12 +8,12 @@
 <!-- wp:wolf-blocks/marquee {"text":"WolfThemes · Premium WordPress Themes · ","direction":"left","animationDuration":30} -->
 <div class="wp-block-wolf-blocks-marquee wolf-blocks-marquee is-dark" style="--wolf-marquee-duration:30s">
 	<div class="wolf-blocks-marquee__track wolf-blocks-marquee__track--left">
-		<span class="wolf-blocks-marquee__item"> WolfThemes ✦ Premium WordPress Themes ✦ </span>
-		<span class="wolf-blocks-marquee__item"> WolfThemes ✦ Premium WordPress Themes ✦ </span>
-		<span class="wolf-blocks-marquee__item"> WolfThemes ✦ Premium WordPress Themes ✦ </span>
-		<span class="wolf-blocks-marquee__item"> WolfThemes ✦ Premium WordPress Themes ✦ </span>
-		<span class="wolf-blocks-marquee__item"> WolfThemes ✦ Premium WordPress Themes ✦ </span>
-		<span class="wolf-blocks-marquee__item"> WolfThemes ✦ Premium WordPress Themes ✦ </span>
+		<span class="wolf-blocks-marquee__item"> WolfThemes <span class="wolf-blocks-marquee__item-separator">✦</span> Premium WordPress Themes <span class="wolf-blocks-marquee__item-separator">✦</span> </span>
+		<span class="wolf-blocks-marquee__item"> WolfThemes <span class="wolf-blocks-marquee__item-separator">✦</span> Premium WordPress Themes <span class="wolf-blocks-marquee__item-separator">✦</span> </span>
+		<span class="wolf-blocks-marquee__item"> WolfThemes <span class="wolf-blocks-marquee__item-separator">✦</span> Premium WordPress Themes <span class="wolf-blocks-marquee__item-separator">✦</span> </span>
+		<span class="wolf-blocks-marquee__item"> WolfThemes <span class="wolf-blocks-marquee__item-separator">✦</span> Premium WordPress Themes <span class="wolf-blocks-marquee__item-separator">✦</span> </span>
+		<span class="wolf-blocks-marquee__item"> WolfThemes <span class="wolf-blocks-marquee__item-separator">✦</span> Premium WordPress Themes <span class="wolf-blocks-marquee__item-separator">✦</span> </span>
+		<span class="wolf-blocks-marquee__item"> WolfThemes <span class="wolf-blocks-marquee__item-separator">✦</span> Premium WordPress Themes <span class="wolf-blocks-marquee__item-separator">✦</span> </span>
 	</div>
 </div>
 <!-- /wp:wolf-blocks/marquee -->
