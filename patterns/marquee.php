2a59c5b7af86bc9bcf9723ddfb1bc8fe76892513
Constantin Saguin <wolfthemes14@gmail.com>
2026-06-19 16:58:09 +0200

    fix: template formatting issue

--- patterns/marquee.php
+++ patterns/marquee.php
@@ -8,7 +8,7 @@
  */
 
 ?>
-<!-- wp:wolf-blocks/marquee {"text":"WolfThemes · Premium WordPress Themes · ","direction":"left","animationDuration":30} -->
+<!-- wp:wolf-blocks/marquee {"text":" WolfThemes <span class=\"wolf-blocks-marquee__item-separator\">✦</span> Premium WordPress Themes <span class=\"wolf-blocks-marquee__item-separator\">✦</span> ","direction":"left","animationDuration":30} -->
 <div class="wp-block-wolf-blocks-marquee wolf-blocks-marquee is-dark" style="--wolf-marquee-duration:30s">
 	<div class="wolf-blocks-marquee__track wolf-blocks-marquee__track--left">
 		<span class="wolf-blocks-marquee__item"> WolfThemes <span class="wolf-blocks-marquee__item-separator">✦</span> Premium WordPress Themes <span class="wolf-blocks-marquee__item-separator">✦</span> </span>
