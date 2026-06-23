fb65fc16b9e46cd6ec6ee449b59bdf18c9a19d4c
Constantin Saguin <wolfthemes14@gmail.com>
2026-06-23 14:04:09 +0200

    Music Themes CTA background

--- patterns/music-themes-cta.php
+++ patterns/music-themes-cta.php
@@ -8,16 +8,16 @@
  */
 
 ?>
-<!-- wp:cover {"url":"<?php echo esc_url( get_theme_file_uri() . '/assets/images/hero-music-themes.jpg' ); ?>","dimRatio":65,"minHeight":440,"minHeightUnit":"px","align":"full","className":"wolf-music-themes-cta is-dark has-texture"} -->
+<!-- wp:cover {"url":"<?php echo esc_url( get_theme_file_uri() . '/assets/images/music-themes-cta-bg.jpg' ); ?>","dimRatio":65,"minHeight":440,"minHeightUnit":"px","align":"full","className":"wolf-music-themes-cta is-dark has-texture"} -->
 <div class="wp-block-cover alignfull wolf-music-themes-cta is-dark has-texture" style="min-height:440px">
 	<span aria-hidden="true" class="wp-block-cover__background has-background-dim-65 has-background-dim"></span>
-	<img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_theme_file_uri() . '/assets/images/hero-music-themes.jpg' ); ?>" width="1673" height="1136" data-object-fit="cover"/>
+	<img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_theme_file_uri() . '/assets/images/music-themes-cta-bg.jpg' ); ?>" width="1673" height="1136" data-object-fit="cover"/>
 
 	<div class="wp-block-cover__inner-container">
 		<!-- wp:group {"layout":{"type":"constrained","contentSize":"820px"}} -->
 		<div class="wp-block-group">
 			<!-- wp:heading {"textAlign":"center","level":2,"fontSize":"2-xl"} -->
-			<h2 class="wp-block-heading has-text-align-center has-2-xl-font-size">34,000+ musicians trust WolfThemes. Build your presence today.</h2>
+			<h2 class="wp-block-heading has-text-align-center has-2-xl-font-size">36,000+ musicians trust WolfThemes. Build your presence today.</h2>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph {"align":"center"} -->
