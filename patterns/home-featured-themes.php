cb6c55821e96033e2c7bde934cb2ebb78f905fbf
Constantin Saguin <wolfthemes14@gmail.com>
2026-06-24 16:28:29 +0200

    fix: set home featured themes count to 15

--- patterns/home-featured-themes.php
+++ patterns/home-featured-themes.php
@@ -11,7 +11,7 @@
 <!-- wp:group {"tagName":"section","className":"wolf-grid","align":"full","layout":{"type":"constrained","contentSize":"var(--wp--style--global--wide-size)"}} -->
 <section class="wp-block-group alignfull wolf-grid">
 
-	<!-- wp:wolf-store/theme-index {"perPage":9,"pagination":"none","orderby":"featured","order":"DESC", "cardHeading":"h2"} /-->
+	<!-- wp:wolf-store/theme-index {"perPage":12,"pagination":"none","orderby":"featured","order":"DESC", "cardHeading":"h2"} /-->
 
 	<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 	<div class="wp-block-buttons">
