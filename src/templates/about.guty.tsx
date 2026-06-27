2c17bdb5e87f1bd8f34e2f9007a8ca618f9f5baf
Constantin Saguin <wolfthemes14@gmail.com>
2026-06-27 03:46:52 +0200

    test: about template redering

--- src/templates/about.guty.tsx
+++ src/templates/about.guty.tsx
@@ -1,9 +1,10 @@
 <Page>
 	<TemplatePart slug="header" tagName="header" area="header" />
-	<Section tagName="main">
+	<Main>
 		<Pattern slug="seijaku-fse/about-intro" />
 		<Pattern slug="seijaku-fse/about-story" />
 		<Pattern slug="seijaku-fse/about-values" />
 		<Pattern slug="seijaku-fse/about-cta" />
-	</Section>
+	</Main>
+	<TemplatePart slug="footer" tagName="footer" area="footer" />
 </Page>
