82faeb4179da5149a64b7c6b516e1518ed8143bd
Constantin Saguin <wolfthemes14@gmail.com>
2026-06-27 03:31:30 +0200

    dev: started guty file and compilation tests

---
+++ src/templates/about.guty.tsx
@@ -0,0 +1,9 @@
+<Page>
+	<TemplatePart slug="header" tagName="header" area="header" />
+	<Section tagName="main">
+		<Pattern slug="seijaku-fse/about-intro" />
+		<Pattern slug="seijaku-fse/about-story" />
+		<Pattern slug="seijaku-fse/about-values" />
+		<Pattern slug="seijaku-fse/about-cta" />
+	</Section>
+</Page>
---
+++ src/templates/front-page.guty.tsx
@@ -0,0 +1,9 @@
+<Page>
+	<TemplatePart slug="header" tagName="header" area="header" theme="seijaku-fse" />
+	<Section tagName="main" layoutType="default">
+		<Pattern slug="seijaku-fse/home-hero" />
+		<Pattern slug="seijaku-fse/home-featured-themes" />
+		<Pattern slug="seijaku-fse/home-about" />
+		<Pattern slug="seijaku-fse/home-music-themes-cta" />
+	</Section>
+</Page>
