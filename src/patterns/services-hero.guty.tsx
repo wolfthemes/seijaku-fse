bc2718ab511acd4c68a1e24eee2b253400282644
Constantin Saguin <wolfthemes14@gmail.com>
2026-06-29 13:08:26 +0200

    compiled templates

--- src/patterns/services-hero.guty.tsx
+++ src/patterns/services-hero.guty.tsx
@@ -7,7 +7,7 @@
 
 <Page>
 	<Cover
-		url={ `<?php echo esc_url( get_theme_file_uri() . '/assets/images/hero-services.jpg' ); ?>` }
+		url="../assets/images/hero-services.jpg"
 		dimRatio={ 40 }
 		minHeight={ 72 }
 		minHeightUnit="vh"
